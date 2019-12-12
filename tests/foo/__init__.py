ab44ae9efdcdb7bfbf5cdc86a13fb09094b11bac
BenjamenMeyer <bmeyer@tripwire.com>
2019-12-12 10:29:13 -0500

    Simply by removing all other configurations
    Show the issue by fixing everything so the only thing that could be
    affecting it is the one specific setting in question.

---
+++ tests/foo/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""
+Dummy Docstring.
+
+Example Test module
+"""
--- tests/foo/test_bar.py
+++ tests/foo/test_bar.py
@@ -1,14 +1,32 @@
-"""Dummy test file."""
+# -*- coding: utf-8 -*-
+
+"""
+Dummy test file.
+
+Example file
+"""
+import unittest
+
 import numpy as np
 
 from foo.bar import bar_function
 
 
 def dummy_function():
-    """Dummy function to use 3rd party lib."""
+    """
+    Show usage of 3rd party lib.
+
+    Returns:
+        NumPy Array
+    """
     return np.array()
 
 
 def test_bar():
-    """Dummy test."""
-    assert bar_function() == 4
+    """
+    Dummy test.
+
+    Raises:
+        Assertion Error if the values do not match
+    """
+    unittest.TestCase.assertEqual(bar_function(), 4)
